72e7211b53ab79b6e2a5d9e932d74ff58d74aee9
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2019-11-06 03:22:33 +0800

    Add files via upload
    main_ubuntu init

---
+++ init_ubuntu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+renice 19 -p $$
+sudo apt-get update
+sudo apt list --upgradable
+cd
+git clone https://github.com/junegunn/fzf.git
+~/fzf/install
+sudo apt install zsh emacs libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ccze  openssh-server build-essential tmux icdiff autojump curl bat fortune-mod magic-wormhole grc wdiff
+source ~/.bashrc
+
+while true; do
+    read -p "Do you wish to change to zsh? Yy/Nn" yn
+    case $yn in
+        [Yy]* ) chsh -s `which zsh`; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo "install oh_my_zsh"
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+curl -sSL https://raw.githubusercontent.com/moonwater/Hello/master/simple_zshrc > .zshrc
