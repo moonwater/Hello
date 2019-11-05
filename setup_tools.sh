6437edd17f2c8db91b1fcbcb8da276d6a46958d7
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2019-11-06 05:05:27 +0800

    Add files via upload
    some tools

---
+++ setup_tools.sh
@@ -0,0 +1,12 @@
+curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py  > /usr/local/bin/speedtest
+chmod +x /usr/local/bin/speedtest
+
+# install icdiff
+curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/icdiff > /usr/local/bin/icdiff
+curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/git-icdiff > /usr/local/bin/git-icdiff
+chmod +x /usr/local/bin/icdiff
+chmod +x /usr/local/bin/git-icdiff
+
+# install lolcat
+curl -sSL https://raw.githubusercontent.com/tehmaze/lolcat/master/lolcat > /usr/local/bin/lolcat
+chmod +x /usr/local/bin/lolcat
