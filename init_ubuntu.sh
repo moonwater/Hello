9ec28a8bf173b94d91483cc821c138fc2ee07705
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2019-11-06 14:57:21 +0800

    Update init_ubuntu.sh
    secure-delete added

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -18,7 +18,7 @@ sudo passwd master
 cd
 git clone https://github.com/junegunn/fzf.git
 ~/fzf/install
-sudo apt install zsh emacs libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ccze openssh-server build-essential tmux icdiff autojump curl fortune-mod magic-wormhole grc wdiff bat git
+sudo apt install zsh emacs libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ccze openssh-server build-essential tmux icdiff autojump curl fortune-mod magic-wormhole grc wdiff bat git secure-delete
 source ~/.bashrc
 
 while true; do
