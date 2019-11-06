de67966702d232f0c2a378427f1b94cc8764a0c5
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2019-11-06 10:35:52 +0800

    Update init_ubuntu.sh
    tuning

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -7,7 +7,7 @@ sudo sed -i '$ a master   ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
 
 echo -n "Enter the password: "
 read -s password
-echo "$password" | passwd master --stdin
+echo 'master:$password' | sudo chpasswd
 
 cd
 git clone https://github.com/junegunn/fzf.git
