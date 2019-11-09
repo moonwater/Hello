5732903f7273e423d5d4e137b7f31f5b8bb049ab
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2019-11-09 17:50:08 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -5,3 +5,6 @@ curl -sSl https://raw.githubusercontent.com/moonwater/Hello/master/init_ubuntu.s
 curl -sSL https://raw.githubusercontent.com/moonwater/Hello/master/simple_zshrc > .zshrc
 curl -sSL https://raw.githubusercontent.com/moonwater/Hello/master/aescrypt_install.sh | bash
 wget -c https://github.com/moonwater/Hello/blob/master/tea.aes\?raw\=true -O .bash_aliases.aes
+
+#remove itself
+rm -- "$0"
