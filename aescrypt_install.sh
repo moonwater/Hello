2e0268b4d10061c8b772d18e4672ff326c8be1f1
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2019-11-18 02:50:27 +0800

    Update aescrypt_install.sh

--- aescrypt_install.sh
+++ aescrypt_install.sh
@@ -1,7 +1,9 @@
+cd
 wget -c https://www.aescrypt.com/download/v3/linux/aescrypt-3.14.tgz
 tar xvf aescrypt-3.14.tgz
 rm -vf aescrypt-3.14.tgz
 cd aescrypt-3.14/src
 make
 sudo make install
-
+cd
+rm -rf aescrypt-3.14
