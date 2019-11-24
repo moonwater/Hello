a535bb760bcdb89e0cfa1984921c196b377c3a18
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2019-11-24 13:59:40 +0800

    Update vv.sh
    removeitself

--- vv.sh
+++ vv.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
- git clone https://github.com/v2ray/v2ray-core.git                                                                                                                           
- cd v2ray-core/                                                                                                                                                              
- cd release/                                                                                                                                                                 
- sudo ./install-release.sh
- cd
+cd
+git clone https://github.com/v2ray/v2ray-core.git                                                                                                                           
+cd v2ray-core/                                                                                                                                                              
+cd release/                                                                                                                                                                 
+sudo ./install-release.sh
+cd
+#remove itself
+rm -- "$0"
