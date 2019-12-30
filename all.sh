111fb583c8f0b4d765328a4f9ad576036de34a5f
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2019-12-30 15:24:30 +0800

    Update all.sh
    download bc and dircolors

--- all.sh
+++ all.sh
@@ -5,6 +5,8 @@ curl -sSl https://raw.githubusercontent.com/moonwater/Hello/master/init_ubuntu.s
 curl -sSL https://raw.githubusercontent.com/moonwater/Hello/master/simple_zshrc > .zshrc
 curl -sSL https://raw.githubusercontent.com/moonwater/Hello/master/aescrypt_install.sh | bash
 wget -c https://github.com/moonwater/Hello/blob/master/tea.aes\?raw\=true -O .bash_aliases.aes
+wget -c https://github.com/moonwater/Hello/blob/master/.bc\?raw\=true -O .bc
+wget -c https://github.com/moonwater/Hello/blob/master/.dircolors\?raw\=true -O .dircolors
 curl -sSl https://raw.githubusercontent.com/moonwater/Hello/master/vv.sh> vv.sh && bash vv.sh
 
 #remove itself
