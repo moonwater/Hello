d7631f90c0bb51641046167bcf7dbedd354c829d
moonwater <c38TeaEGe4ZhNeR@gmail.com>
2022-02-06 13:15:57 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -9,7 +9,7 @@ wget -c https://github.com/moonwater/Hello/blob/master/.bc\?raw\=true -O .bc
 wget -c https://github.com/moonwater/Hello/blob/master/.dircolors\?raw\=true -O .dircolors
 wget -c https://github.com/moonwater/Hello/blob/master/.vimrc\?raw\=true -O .vimrc
 wget -c https://github.com/moonwater/Hello/blob/master/.emacs\?raw\=true -O .emacs
-curl -sSl https://raw.githubusercontent.com/moonwater/Hello/master/vv.sh> vv.sh && bash vv.sh
+curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh > vv.sh && sudo bash vv.sh 
 
 #remove itself
 rm -- "$0"
